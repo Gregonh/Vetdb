a63f18492d8a43dafc89fcb2d246acc0d4a4921c
grego_nh <gregonh@gmail.com>
2024-07-10 11:28:08 +0200

    Adding several components New components from shadui to use them in the project also adding modifications in package.json server and client.

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,23 +1,58 @@
-import { useState } from 'react';
-
 // eslint-disable-next-line import/no-unresolved
-import { Button } from '@/components/ui/button';
+import { Button } from '../components/ui/button';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '../components/ui/card';
+import { Input } from '../components/ui/input';
+import { Label } from '../components/ui/label';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '../components/ui/select';
 
 function Home() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <h1>Vite + React</h1>
-      <div>
-        <Button
-          onClick={() => setCount((count) => count + 1)}
-          className="bg-blue-400 text-red-900"
-        >
-          count is {count}
-        </Button>
-      </div>
-    </>
+    <Card className="w-[350px]">
+      <CardHeader>
+        <CardTitle>Create project</CardTitle>
+        <CardDescription>Deploy your new project in one-click.</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form>
+          <div className="grid w-full items-center gap-4">
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="name">Name</Label>
+              <Input id="name" placeholder="Name of your project" />
+            </div>
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="framework">Framework</Label>
+              <Select>
+                <SelectTrigger id="framework">
+                  <SelectValue placeholder="Select" />
+                </SelectTrigger>
+                <SelectContent position="popper">
+                  <SelectItem value="next">Next.js</SelectItem>
+                  <SelectItem value="sveltekit">SvelteKit</SelectItem>
+                  <SelectItem value="astro">Astro</SelectItem>
+                </SelectContent>
+              </Select>
+            </div>
+          </div>
+        </form>
+      </CardContent>
+      <CardFooter className="flex justify-between">
+        <Button variant="outline">Cancel</Button>
+        <Button>Deploy</Button>
+      </CardFooter>
+    </Card>
   );
 }
 
--- server/src/index.js
+++ server/src/index.js
@@ -4,23 +4,46 @@
 import bodyParser from 'body-parser';
 import errorhandler from 'errorhandler';
 import express from 'express';
+import { Pool } from 'pg';
 
 const app = express();
 //where to listen for new requests by providing a port number
 const PORT = process.env.PORT || 4001;
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  }),
+);
+
+const pool = new Pool({
+  user: 'postgres', // replace with your database username
+  host: 'localhost', // replace with your database host
+  database: 'dvdrental', // replace with your database name
+  password: 'libretita5445.', // replace with your database password
+  port: 5432, // replace with your database port
+});
 
 // Use static server to serve the Express Yourself Website
 app.use(express.static('public'));
-
-app.use(bodyParser.json());
 
 app.use((req, res, next) => {
   console.log(`${req.method} Request Received`);
   next();
 });
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+//query function to call in the endpoint
+const getUsers = (request, response) => {
+  pool.query('SELECT * FROM users ORDER BY id ASC', (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(200).json(results.rows);
+  });
+};
+
+app.get('/', (request, response) => {
+  response.json({ info: 'Node.js, Express, and Postgres API' });
 });
 
 if (process.env.NODE_ENV === 'development') {
